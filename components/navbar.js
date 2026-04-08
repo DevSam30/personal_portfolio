e7a075daee065580cba59cb895b5df69f76be288
Sam Kamau <skmbugua5@gmail.com>
2026-04-08 17:14:08 +0300

    Added contacts page

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(){
         <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
             <Link href="/">Home</Link> |{" "}
             <Link href="/about">About</Link> |{" "}
-            <Link href="/projectss">Projects</Link> |{" "}
+            <Link href="/projects">Projects</Link> |{" "}
             <Link href="/contacts">Contacts</Link>
         </nav>
     );
